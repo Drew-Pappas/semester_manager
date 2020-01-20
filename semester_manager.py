3228aaa30cd85fe10c05024592fd1dacaf4c2f8a
Drew-Pappas <41135406+Drew-Pappas@users.noreply.github.com>
2020-01-20 16:12:51 -0500

    initial commit with some code/directory structure for the tool

---
+++ semester_manager.py
@@ -0,0 +1,57 @@
+import pathlib, os
+
+def new_folder():
+    '''
+    '''
+    #Creates a new folder in the specified directory
+
+def make_bookmark():
+    '''
+    '''
+    #Creates a bookmark in chrome for the specified course
+
+def send_bookmark_to_chrome():
+    '''
+    '''
+    #Exports created bookmark to chrome tree
+
+def course_name():
+    '''
+    '''
+    #Accepts user course name + number and returns a string in a standardized format
+
+def create_config_file():
+    '''
+    '''
+    #Takes user input for the classes for the semester and exports
+    #it to an editable text file 
+
+def main():
+    '''
+    '''
+    #Entry point into the program
+
+    #Check for config file
+
+    #Validate config file if alterations were made
+
+    #If no config file found, runs user through first time process
+
+    #Ask user for the semester + year
+
+    #Create folder for current semester/year
+
+    #Ask user for number of classes
+
+    #Ask user for class abbreviation and class number
+
+    #Ask user for number of meetings per week
+
+    #Create folder structure for that course
+
+    #Repeat for specified number of courses in the semester
+
+    #
+
+if __name__ == "__main__":
+    main()
